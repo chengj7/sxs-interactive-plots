65c5141bc39a9db0fda9273b8933f17448afd199
chengj7 <137737242+chengj7@users.noreply.github.com>
2025-11-16 13:58:47 -0500

    Fix typo in DataFrame initialization

--- plots/isxs_marimo.py
+++ plots/isxs_marimo.py
@@ -121,7 +121,7 @@ def iplt_lm(xStrain, yStrain, hlm, Mass, Distance):
 def make_markdown(metadata_list, dropdown, idx):
     cat = ["n orbits", "mass ratio", "eccentricity", "chi1", "chi2", "chi1_perp", "chi2_perp"]
     line1 = mo.md(f"""<h1 style="font-size: 24px;">{dropdown.value} Metadata:</h1>""")
-    df = pd.Dataframe({"Parameter": [cat[i] for i in range(len(cat))], "Value": [metadata_list[idx][1][j] for j in range(len(metadata_list[idx][1]))]})
+    df = pd.DataFrame({"Parameter": [cat[i] for i in range(len(cat))], "Value": [metadata_list[idx][1][j] for j in range(len(metadata_list[idx][1]))]})
     transformed_df = mo.ui.dataframe(df)
     markdown = mo.vstack([line1, mo.md("-----------"), transformed_df])
     return markdown
