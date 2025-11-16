ae4697d559538d7798b27e3d110665be48560cd7
chengj7 <137737242+chengj7@users.noreply.github.com>
2025-11-16 15:10:08 -0500

    Consolidate dropdowns into tabbed interface
    Refactor mass ratio, eccentricity, and precession dropdowns into a single function with tabs for better organization.

--- plots/SXSInteractivePlots.py
+++ plots/SXSInteractivePlots.py
@@ -66,28 +66,47 @@ def _(mo):
 
 @app.cell
 def _(mo):
-    mo.md(r"""# Varying <span style="color:red">Mass Ratios</span> Examples #""")
-    return
-
-
+    tabs = mo.ui.tabs({
+    "Mass Ratio": mo.md(r"""# Varying <span style="color:red">Mass Ratios</span> Examples #"""),
+    "Eccentricity": mo.md(r"""# High <span style="color:green">Eccentricity</span> Examples #"""),
+    "Precession": mo.md(r"""# High <span style="color:lightblue">Precession</span> Examples #""")
+    })
+    tabs
+    return tabs
+    
 @app.cell
 def _(mo):
-    dropdown_MR = mo.ui.dropdown(
-        options=["SXS:BBH:1154 (MR:1)", "SXS:BBH:2139 (MR:3)", "SXS:BBH:1441 (MR:8)", "SXS:BBH:1107 (MR:10)"],
-        value="SXS:BBH:1154 (MR:1)",
-        label="Choose a Mass Ratio",
-        searchable=True,
-    )
-    Distance_MR = mo.ui.slider(100.0,10000.0,10.0, label="Distance (Mpc)", include_input=True, full_width=True)
-    Mass_MR = mo.ui.slider(5.0,10000.0,1.0, label="Mass (Solar Mass M☉)", value=33 ,include_input=True, full_width=True)
-    return Distance_MR, Mass_MR, dropdown_MR
+    if tabs.value == "Mass Ratio":
+        dropdown_ = mo.ui.dropdown(
+            options=["SXS:BBH:1154 (MR:1)", "SXS:BBH:2139 (MR:3)", "SXS:BBH:1441 (MR:8)", "SXS:BBH:1107 (MR:10)"],
+            value="SXS:BBH:1154 (MR:1)",
+            label="Choose a Mass Ratio",
+            searchable=True,
+        )
+    elif tabs.value == "Eccentricity":
+        dropdown = mo.ui.dropdown(
+            options=["SXS:BBH:2527 (MR:1)", "SXS:BBH:3946 (MR:2)", "SXS:BBH:2550 (MR:4)", "SXS:BBH:2557 (MR:6)", "SXS:BBH:2595 (MR:1)", "SXS:BBH:2537 (MR:3)", "SXS:BBH:2553 (MR:6)", "SXS:BBH:2560 (MR:8)"],
+            value="SXS:BBH:2527 (MR:1)",
+            label="Choose a system:",
+            searchable=True,
+        )
+    elif tabs.value == "Precession":
+        dropdown = mo.ui.dropdown(
+            options=["SXS:BBH:2442 (MR:1)", "SXS:BBH:2443 (MR:1)", "SXS:BBH:0832 (MR:2)"],
+            value="SXS:BBH:2442 (MR:1)",
+            label="Choose a system:",
+            searchable=True,
+        )
+    Distance = mo.ui.slider(100.0,10000.0,10.0, label="Distance (Mpc)", include_input=True, full_width=True)
+    Mass = mo.ui.slider(5.0,10000.0,1.0, label="Mass (Solar Mass M☉)", value=33 ,include_input=True, full_width=True)
+    return Distance, Mass, dropdown
 
 
 @app.cell
 def _(
-    Distance_MR,
-    Mass_MR,
-    dropdown_MR,
+    Distance,
+    Mass,
+    dropdown,
     h_id_list,
     hlm,
     isxs,
@@ -97,79 +116,9 @@ def _(
     isxs.run(dropdown_MR.value[:12], h_id_list, strain_data, metadata_list, hlm, Mass_MR, Distance_MR, dropdown_MR, show_CE, show_aLIGO)
     return
 
-
-@app.cell
-def _(mo):
-    mo.md(r"""# High <span style="color:green">Eccentricity</span> Examples #""")
-    return
-
-
-@app.cell
-def _(mo):
-    dropdown_ecc = mo.ui.dropdown(
-        options=["SXS:BBH:2527 (MR:1)", "SXS:BBH:3946 (MR:2)", "SXS:BBH:2550 (MR:4)", "SXS:BBH:2557 (MR:6)", "SXS:BBH:2595 (MR:1)", "SXS:BBH:2537 (MR:3)", "SXS:BBH:2553 (MR:6)", "SXS:BBH:2560 (MR:8)"],
-        value="SXS:BBH:2527 (MR:1)",
-        label="Choose a system:",
-        searchable=True,
-    )
-    Distance_ecc = mo.ui.slider(100.0,10000.0,10.0, label="Distance (Mpc)", include_input=True, full_width=True)
-    Mass_ecc = mo.ui.slider(5.0,10000.0,1.0, label="Mass (Solar Mass M☉)", value=33 ,include_input=True, full_width=True)
-    return Distance_ecc, Mass_ecc, dropdown_ecc
-
-
-@app.cell
-def _(
-    Distance_ecc,
-    Mass_ecc,
-    dropdown_ecc,
-    h_id_list,
-    hlm,
-    isxs,
-    metadata_list,
-    strain_data,
-):
-    isxs.run(dropdown_ecc.value[:12], h_id_list, strain_data, metadata_list, hlm, Mass_ecc, Distance_ecc, dropdown_ecc, show_CE, show_aLIGO)
-    return
-
-
-@app.cell
-def _(mo):
-    mo.md(r"""# High <span style="color:lightblue">Precession</span> Examples #""")
-    return
-
-
-@app.cell
-def _(mo):
-    dropdown_prec = mo.ui.dropdown(
-        options=["SXS:BBH:2442 (MR:1)", "SXS:BBH:2443 (MR:1)", "SXS:BBH:0832 (MR:2)"],
-        value="SXS:BBH:2442 (MR:1)",
-        label="Choose a system:",
-        searchable=True,
-    )
-    Distance_prec = mo.ui.slider(100.0,10000.0,10.0, label="Distance (Mpc)", include_input=True, full_width=True)
-    Mass_prec = mo.ui.slider(5.0,10000.0,1.0, label="Mass (Solar Mass M☉)", value=33 ,include_input=True, full_width=True)
-    return Distance_prec, Mass_prec, dropdown_prec
-
-
-@app.cell
-def _(
-    Distance_prec,
-    Mass_prec,
-    dropdown_prec,
-    h_id_list,
-    hlm,
-    isxs,
-    metadata_list,
-    strain_data,
-):
-    isxs.run(dropdown_prec.value[:12], h_id_list, strain_data, metadata_list, hlm, Mass_prec, Distance_prec, dropdown_prec, show_CE, show_aLIGO)
-    return
-
-
 @app.cell
 def _():
     return
-
 
 if __name__ == "__main__":
     app.run()
